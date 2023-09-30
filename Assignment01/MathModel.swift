77e0d671e68a6ed296f45982208aed6fd092ed42
simon <simonangelfong@gmail.com>
2023-09-30 09:48:46 -0400

    Initial Commit

---
+++ Assignment01/MathModel.swift
@@ -0,0 +1,12 @@
+//
+//  MathModel.swift
+//  Assignment01
+//
+//  Created by Simon Fong on 2023-09-29.
+//
+
+import Foundation
+
+class MathModel{
+    
+}
---
+++ Assignment01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment01
+//
+//  Created by Simon Fong on 2023-09-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
